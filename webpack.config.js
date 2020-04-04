6baf7bbc5c3413dd55febcf3c8cb60f7809eff7c
JieZhan <178889339@qq.com>
2020-04-04 16:52:04 +0800

    第一次提交

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[contenthash].js'
+    },
+    plugins: [new HtmlWebpackPlugin({
+        title: "詹杰",
+        template: "./src/asset/index.html"
+    })],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    }
+};
